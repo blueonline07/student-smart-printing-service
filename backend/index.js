01f3db129d27e3c020bc5e0fba7134eba43a242a
blueonline07 <ldkhang1201@gmail.com>
2024-11-21 21:41:46 +0700

    feat: add users CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
 // server.js
 import express from 'express';
+import cors from 'cors';
+import userRoutes from './routes/UserRoutes.js';
+import printerRoutes from './routes/PrinterRoutes.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000', // Adjust this to match your frontend's domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Adjust allowed HTTP methods if necessary
+  allowedHeaders: ['*'], // Adjust allowed headers
+  credentials: true,
+}));
 
-app.get('/test', (req, res) => {
-  res.send('Hello, test!');
-});
+app.use('/users', userRoutes)
+// app.use('/printers', printerRoutes)
 
 // Start server
 app.listen(PORT, () => {
