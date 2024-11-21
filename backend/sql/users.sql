18e2ceecb6b3a4897695b43a4ad5920da0d4a2b9
blueonline07 <ldkhang1201@gmail.com>
2024-11-22 00:31:58 +0700

    feat: add printer CRUD

---
+++ backend/sql/users.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users (
+  email VARCHAR(100) PRIMARY KEY,
+  name NVARCHAR(100) NOT NULL
+);
+
+CREATE TABLE students (
+  email VARCHAR(100) PRIMARY KEY REFERENCES users(email) ON DELETE CASCADE,
+  balance INT NOT NULL
+);
+
+CREATE TABLE staffs (
+  email VARCHAR(100) PRIMARY KEY REFERENCES users(email) ON DELETE CASCADE,
+  workplace VARCHAR(50) NOT NULL
+  -- room CHAR(3) NOT NULL,
+  -- building CHAR(2) NOT NULL,
+  -- campus CHAR(3) NOT NULL
+);
