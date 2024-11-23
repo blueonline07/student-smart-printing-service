378dd8e750b95705b2a6802ad686233e10db3b2f
Minhiin <167101474+minie111204@users.noreply.github.com>
2024-11-23 20:57:28 +0700

    feat: add HomeAfterLogin page

--- frontend/mobile/App.js
+++ frontend/mobile/App.js
@@ -2,27 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeBeforeLogin from './HomeBeforeLogin.js';  
-import HomeLogin from './HomeLogin.js';  
+
+// Import your screens
+import HomeBeforeLogin from './HomeBeforeLogin';
+import HomeLogin from './HomeLogin';
+import HomeAfterLogin from './HomeAfterLogin';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeBeforeLogin"> 
+      <Stack.Navigator initialRouteName="HomeBeforeLogin">
         
-        {/* HomeBeforeLogin */}
-        <Stack.Screen 
-          name="HomeBeforeLogin" 
-          component={HomeBeforeLogin} 
+        {/* HomeBeforeLogin Screen */}
+        <Stack.Screen
+          name="HomeBeforeLogin"
+          component={HomeBeforeLogin}
           options={{ headerShown: false }}
         />
 
-        {/* HomeLogin */}
-        <Stack.Screen 
-          name="HomeLogin" 
-          component={HomeLogin} 
+        {/* HomeLogin Screen */}
+        <Stack.Screen
+          name="HomeLogin"
+          component={HomeLogin}
+          options={{ headerShown: false }}
+        />
+
+        {/* HomeAfterLogin Screen */}
+        <Stack.Screen
+          name="HomeAfterLogin"
+          component={HomeAfterLogin}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
